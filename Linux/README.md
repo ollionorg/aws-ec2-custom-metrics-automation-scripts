f9d18481fa86e9e23cf73a4e86200055a237977d
pareshcc <paresh@cldcvr.com>
2023-04-07 10:35:54 +0530

    update-script-as-per-comments

--- Linux/README.md
+++ Linux/README.md
@@ -1,5 +1,8 @@
 # Custom Metrics Installation for Linux Instances
-This document provides instructions for installing the custom-metrics-disk-memory-linux.sh script on any Linux instance. The script collects and publishes custom metrics for disk usage and memory usage to Amazon CloudWatch.
+This document provides instructions for installing the `auto-install-custom-metrics-linux.sh` script on any Linux instance. The script collects and publishes custom metrics for disk usage and memory usage to Amazon CloudWatch.
+
+## Note: 
+* The `auto-install-custom-metrics-linux.sh` script supports both IMDSv1 and IMDSv2. It is recommended to use IMDSv2 for improved security.
 
 ## Prerequisites
 Before you begin, ensure you have the following:
@@ -32,16 +35,18 @@ Alternatively, you can use the following base64-encoded script in the user data:
 ```
 IyEvYmluL2Jhc2gKY3VybCAtTyBodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vY2xkY3ZyL2F1dG9tYXRlLWN1c3RvbS1tZXRyaWNzLWluc3RhbGxhdGlvbi9tYWluL0xpbnV4L2F1dG8taW5zdGFsbC1jdXN0b20tbWV0cmljcy1saW51eC5zaApiYXNoIC14IGF1dG8taW5zdGFsbC1jdXN0b20tbWV0cmljcy1saW51eC5zaCA+IC92YXIvbG9nL2F1dG8taW5zdGFsbC1jdXN0b20tbWV0cmljcy1saW51eC5zaC5sb2cgMj4mMQo=
 ```
+
 ## Note
-* The installation auto-install-custom-metrics-linux.sh script is generic and will run on any Linux instance.
-* The installation auto-install-custom-metrics-linux.sh script includes all disks and mount points in the Linux system.
-* The installation auto-install-custom-metrics-linux.sh script will take the DiskMetric and MemoryMetric arguments.
+* The script is generic (will run on any Linux instance)
+* This script will include all the disks and mount points in the linux system.
+* The same script will take argument `MemoryMetric` and `DiskMetric`.
+* The `auto-install-custom-metrics-linux.sh` script will implement all the steps above, automatically.
 
 We recommend reviewing the script before running it to understand what it does.
 
 
 # Create Alarms for Windows and Linux Instances on AWS EC2
-The auto-custom-metrics-alarms.sh script creates CloudWatch alarms for custom metrics on an EC2 instance. The script checks whether the instance is running on Windows or Linux and then sets alarms for the available custom metrics accordingly. The alarms are created based on thresholds for certain metric values, and the script sends notifications to an SNS topic when an alarm is triggered.
+The `auto-custom-metrics-alarms.sh` script creates CloudWatch alarms for custom metrics on an EC2 instance. The script checks whether the instance is running on Windows or Linux and then sets alarms for the available custom metrics accordingly. The alarms are created based on thresholds for certain metric values, and the script sends notifications to an SNS topic when an alarm is triggered.
 
 ## Prerequisites
 Before you begin, ensure you have the following:
@@ -55,12 +60,13 @@ Before you begin, ensure you have the following:
 The script takes four arguments as input:
 
 ```
-bash auto-custom-metrics-alarms.sh INSTANCE_ID ENVIRONMENT AWS_REGION SNS_ARN
+bash auto-custom-metrics-alarms.sh INSTANCE_ID ENVIRONMENT AWS_REGION SNS_ARN PERCENTAGE_VALUE
 ```
 
 * `INSTANCE_ID`: The ID of the EC2 instance for which you want to set alarms.
 * `ENVIRONMENT`: The environment in which the instance is running (e.g., production, staging).
 * `AWS_REGION`: The AWS region in which the instance is running.
 * `SNS_ARN`: The Amazon Resource Name (ARN) of the SNS topic to which you want to send notifications.
+* `PERCENTAGE_VALUE`: The percentage threshold for CPU utilization that triggers the alarm. When the average CPU utilization of the instance exceeds this value for a specified period of time, the alarm will be triggered and a notification will be sent to the specified SNS topic.
 
 It is recommended that you review the `auto-custom-metrics-alarms.sh` script before running it to understand what it does.
--- Linux/auto-custom-metrics-alarms.sh
+++ Linux/auto-custom-metrics-alarms.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-if [ $# -ne 4 ]; then
-    echo "Usage: $0 INSTANCE_ID ENVIRONMENT AWS_REGION SNS_ARN"
+if [ $# -ne 5 ]; then
+    echo "Usage: $0 INSTANCE_ID ENVIRONMENT AWS_REGION SNS_ARN PERCENTAGE_VALUE"
+    exit 1
+fi
+
+# Check if the instance ID exists
+if ! aws ec2 describe-instances --region $3 --instance-id "$1" >/dev/null 2>&1; then
+    echo "Instance with ID $1 does not exist in your AWS account."
+    exit 1
+fi
+
+# Check if SNS topic exists in AWS account
+if ! aws sns list-topics --region $3 --output text | grep -q "$4"; then
+    echo "SNS topic not found in AWS account. Please check the SNS ARN and try again."
     exit 1
 fi
 
@@ -9,9 +21,10 @@ INST_ID="$1"
 ENV="$2"
 AWS_REGION="$3"
 SNS_ARN="$4"
+PERCENTAGE_VALUE="$5"
 
 echo "Checking custom metrics associated with $INST_ID"
-if [ -z "$(aws ec2 describe-instances --instance-id $INST_ID --output text --query 'Reservations[*].Instances[*].Platform')" ]; then
+if [ -z "$(aws ec2 describe-instances --instance-id $INST_ID --region $AWS_REGION --output text --query 'Reservations[*].Instances[*].Platform')" ]; then
         echo "Instance Platform is Linux"
         INST_PLATFORM="Linux"
 else
@@ -23,13 +36,13 @@ echo "Checking for available Custom Metrics"
 
 setDiskAlarmsLinux() {
     # Main logic #setDiskAlarms "$SNS_ARN" "$NAMESPACE_ID" "$METRIC_NAME""Utilization"
-    aws cloudwatch list-metrics --output text --query 'Metrics[?Namespace==`'"$2"'`].Dimensions' | tr -s '\t' ':' | paste -d, - - - | grep "$INST_ID" | while read -r DISK_DIMENSIONS; do
+    aws cloudwatch list-metrics --region $AWS_REGION --output text --query 'Metrics[?Namespace==`'"$2"'`].Dimensions' | tr -s '\t' ':' | paste -d, - - - | grep "$INST_ID" | while read -r DISK_DIMENSIONS; do
         DRIVE_ID="$(echo $DISK_DIMENSIONS | awk -F: '{print $NF}')"
         MOUNT_PATH="$(echo $DISK_DIMENSIONS | awk -F'[,:]+' '{print $2}')"
         INST_NAME="$(aws ec2 describe-instances --instance-ids $INST_ID --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value]' --output text)"
 
         # Loop through percentage values
-        for PERCENTAGE in 90 80; do
+        for PERCENTAGE in $PERCENTAGE_VALUE; do
             # Build alarm description
             ALARM_NAME="$ENV-$INST_NAME-$INST_ID-$DRIVE_ID-$3-$PERCENTAGE"
             ALARM_DESCRIPTION="$ENV $INST_NAME $INST_ID $DRIVE_ID: $3 > $PERCENTAGE%"
@@ -56,10 +69,10 @@ setDiskAlarmsLinux() {
 
 setMemoryAlarms() {
     # Main logic
-    aws cloudwatch list-metrics --output text --query 'Metrics[?Namespace==`'"$2"'`].Dimensions[].Value' | tr -s '\t' '\n' | grep -e "$INST_ID" | while read -r INST_ID; do
+    aws cloudwatch list-metrics --region $AWS_REGION --output text --query 'Metrics[?Namespace==`'"$2"'`].Dimensions[].Value' | tr -s '\t' '\n' | grep -e "$INST_ID" | while read -r INST_ID; do
         IFS=,
-        aws ec2 describe-instances --instance-ids "$INST_ID" --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value]' --output text | while read -r INST_NAME; do
-            for PERCENTAGE in 90; do
+        aws ec2 describe-instances --instance-ids "$INST_ID" --region $AWS_REGION --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value]' --output text | while read -r INST_NAME; do
+            for PERCENTAGE in $PERCENTAGE_VALUE; do
                 ALARM_DESCRIPTION="$ENV $INST_NAME $INST_ID $3 > $PERCENTAGE%"
                 aws cloudwatch put-metric-alarm \
                     --alarm-name "$ENV-$INST_NAME-$INST_ID-$3-$PERCENTAGE" \
@@ -74,7 +87,8 @@ setMemoryAlarms() {
                     --period 300 \
                     --statistic Average \
                     --threshold "$PERCENTAGE" \
-                    --unit Percent
+                    --unit Percent \
+                    --region $AWS_REGION
             done
         done
     done
@@ -84,7 +98,7 @@ setMemoryAlarms() {
 if [ "$INST_PLATFORM" == "Windows" ]; then
         for NAMESPACE_ID in "$INST_PLATFORM"/Disk "$INST_PLATFORM"/Memory; do
                 export METRIC_NAME="$(echo "$NAMESPACE_ID" | cut -d/ -f2)"
-                if aws cloudwatch list-metrics --output text --query 'Metrics[?Namespace==`'"$NAMESPACE_ID"'`].Dimensions[].Value' | tr -s '\t' '\n' | grep -q "$INST_ID"; then
+                if aws cloudwatch list-metrics --region $AWS_REGION --output text --query 'Metrics[?Namespace==`'"$NAMESPACE_ID"'`].Dimensions[].Value' | tr -s '\t' '\n' | grep -q "$INST_ID"; then
                         echo "Custom Metrics present for $NAMESPACE_ID; Setting Alerts"
                         if [ "$METRIC_NAME" == "Disk" ]; then
                                 setDiskAlarmsWindows "$SNS_ARN" "$NAMESPACE_ID" "$METRIC_NAME""Utilization"
@@ -98,7 +112,7 @@ if [ "$INST_PLATFORM" == "Windows" ]; then
 elif [[ "$INST_PLATFORM" == "Linux" ]]; then
         for NAMESPACE_ID in "$INST_PLATFORM"/Disk "$INST_PLATFORM"/Memory; do
                 export METRIC_NAME="$(echo "$NAMESPACE_ID" | cut -d/ -f2)"
-                if aws cloudwatch list-metrics --output text --query 'Metrics[?Namespace==`'"$NAMESPACE_ID"'`].Dimensions[].Value' | tr -s '\t' '\n' | grep -q "$INST_ID"; then
+                if aws cloudwatch list-metrics --region $AWS_REGION --output text --query 'Metrics[?Namespace==`'"$NAMESPACE_ID"'`].Dimensions[].Value' | tr -s '\t' '\n' | grep -q "$INST_ID"; then
                         echo "Custom Metrics present for $NAMESPACE_ID; Setting Alerts"
                         if [ "$METRIC_NAME" == "Disk" ]; then
                                 setDiskAlarmsLinux "$SNS_ARN" "$NAMESPACE_ID" "$METRIC_NAME""Utilization"
